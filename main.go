4e044e53fd31a719f67d6d89c0d713c1774ec16d
CHENQ <chennqqi@qq.com>
2016-12-26 10:36:37 +0800

    1

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"math"
 	"regexp"
 	"strings"
 	"time"
@@ -15,11 +16,12 @@ import (
 
 var (
 	ErrNotMatch = errors.New("Not Matched")
+	verbose     = 0
 )
 
 type Result struct {
 	Language string
-	Score    float64
+	Score    int
 }
 
 type CodeDetector interface {
@@ -39,9 +41,27 @@ func LoadRules(rulename string) (CodeDetector, error) {
 		return nil, err
 	}
 	for idx := 0; idx < len(sd.dict); idx++ {
+		for jdx := 0; jdx < len(sd.dict[idx].TopRules); jdx++ {
+			if verbose > 2 {
+				fmt.Println(sd.dict[idx].Language, "TopRules:", sd.dict[idx].TopRules[jdx].Value)
+			}
+			sd.dict[idx].TopRules[jdx].exp = regexp.MustCompile(
+				sd.dict[idx].TopRules[jdx].Value)
+		}
+		for jdx := 0; jdx < len(sd.dict[idx].NearTopRules); jdx++ {
+			sd.dict[idx].NearTopRules[jdx].exp = regexp.MustCompile(
+				sd.dict[idx].NearTopRules[jdx].Value)
+			if verbose > 2 {
+				fmt.Println(sd.dict[idx].Language, "NearTopRules:", sd.dict[idx].NearTopRules[jdx].Value)
+			}
+		}
+
 		for jdx := 0; jdx < len(sd.dict[idx].Rules); jdx++ {
 			sd.dict[idx].Rules[jdx].exp = regexp.MustCompile(
 				sd.dict[idx].Rules[jdx].Value)
+			if verbose > 2 {
+				fmt.Println(sd.dict[idx].Language, "Rules:", sd.dict[idx].Rules[jdx].Value)
+			}
 		}
 	}
 	return &sd, nil
@@ -56,100 +76,153 @@ func (sd *ScriptCodeDetector) GetRuleIndex() []string {
 }
 
 func (sd *ScriptCodeDetector) Detect(txt []byte) (Result, error) {
-	for idx := 0; idx < len(sd.dict); idx++ {
-		var r Result
-		r.Language = sd.dict[idx].Language
-		for jdx := 0; jdx < len(sd.dict[idx].Rules); jdx++ {
-			if sd.dict[idx].Rules[jdx].exp.Match(txt) {
-				r.Score += sd.dict[idx].Rules[jdx].Score
-			}
-			if r.Score >= 1.0 {
-				return r, nil
-			}
-		}
-	}
+	//	for idx := 0; idx < len(sd.dict); idx++ {
+	//		var r Result
+	//		r.Language = sd.dict[idx].Language
+	//		for jdx := 0; jdx < len(sd.dict[idx].Rules); jdx++ {
+	//			if sd.dict[idx].Rules[jdx].exp.Match(txt) {
+	//				r.Score += sd.dict[idx].Rules[jdx].Score
+	//			}
+	//			if r.Score >= 1.0 {
+	//				return r, nil
+	//			}
+	//		}
+	//	}
 	return Result{}, ErrNotMatch
 }
 
 func (sd *ScriptCodeDetector) DetectAll(txt []byte) ([]Result, error) {
 	var r []Result
 
-	for idx := 0; idx < len(sd.dict); idx++ {
-		var t Result
-		t.Language = sd.dict[idx].Language
-		for jdx := 0; jdx < len(sd.dict[idx].Rules); jdx++ {
-			if sd.dict[idx].Rules[jdx].exp.Match(txt) {
-				t.Score += sd.dict[idx].Rules[jdx].Score
-			}
-			if t.Score >= 1.0 {
-				r = append(r, t)
-				break
-			} else if t.Score <= -1.0 {
-				break
-			}
-		}
-	}
-	if len(r) > 0 {
-		return r, nil
-	}
+	//	for idx := 0; idx < len(sd.dict); idx++ {
+	//		var t Result
+	//		t.Language = sd.dict[idx].Language
+	//		for jdx := 0; jdx < len(sd.dict[idx].Rules); jdx++ {
+	//			if sd.dict[idx].Rules[jdx].exp.Match(txt) {
+	//				t.Score += sd.dict[idx].Rules[jdx].Score
+	//			}
+	//			if t.Score >= 1.0 {
+	//				r = append(r, t)
+	//				break
+	//			} else if t.Score <= -1.0 {
+	//				break
+	//			}
+	//		}
+	//	}
+	//	if len(r) > 0 {
+	//		return r, nil
+	//	}
 	return r, ErrNotMatch
 }
 
-func (sd *ScriptCodeDetector) DetectBest(txt []byte) (Result, error) {
-	var r []Result
-	for idx := 0; idx < len(sd.dict); idx++ {
-		var t Result
-		t.Language = sd.dict[idx].Language
-		for jdx := 0; jdx < len(sd.dict[idx].Rules); jdx++ {
-			if sd.dict[idx].Rules[jdx].exp.Match(txt) {
-				t.Score += sd.dict[idx].Rules[jdx].Score
+func doCalScore(rules []RuleItem, line []byte) int {
+	var score int
+	for _, rule := range rules {
+		if verbose > 1 {
+			fmt.Print("match:", string(line), rule.Value)
+		}
+		if rule.exp.Match(line) {
+			score += rule.Score
+			if verbose > 1 {
+				fmt.Println(" OK")
 			}
-			if t.Score <= -1.0 {
-				break
+		} else {
+			if verbose > 1 {
+				fmt.Println(" NOT")
 			}
 		}
 	}
-	var maxIdx int
-	for idx := 1; idx < len(r); idx++ {
-		if r[idx].Score > r[maxIdx].Score {
-			maxIdx = idx
-		}
-	}
-
-	return r[maxIdx], nil
+	return score
 }
 
-func (sd *ScriptCodeDetector) DetectBest2(txt []byte) (Result, error) {
-	newTxt := bytes.Replace(txt, []byte("\r\n"), []byte("\n"))
-	linesofCode := bytes.Split(newTxt, []byte("\n"))
-	linenos := len(linesofCode)
-	neartop := func(line int) {
-		if linenos <= 10 {
-			return true
-		}
-		return line < linenos/10
+func (sd *ScriptCodeDetector) DetectBest(txt []byte) (Result, error) {
+	var idxStart int
+	//skip UTF-8 EFBBBF
+	if len(txt) >= 3 && txt[0] == 0xEF && txt[1] == 0xBB && txt[2] == 0xBF {
+		txt = txt[3:]
 	}
 
+	for idx := 0; idx < len(txt); idx++ {
+		switch txt[idx] {
+		case '\t', ' ', '\r', '\n':
+			//donothing
+
+		default:
+			idxStart = idx
+			goto BREAK_FOR
+		}
+	}
+BREAK_FOR:
+
+	newTxt := bytes.TrimFunc(txt[idxStart:], func(b rune) bool {
+		return uint32(b) == '\r'
+	})
+
+	linesofCode := bytes.Split(newTxt, []byte("\n"))
+	linecount := len(linesofCode)
+	neartop := func(line int) bool {
+		if linecount <= 10 {
+			return true
+		}
+		return line < linecount/10
+	}
+
+	valididxMap := make(map[int]int)
+	for idx := 0; idx < linecount; idx++ {
+		if neartop(idx) {
+			valididxMap[idx] = 1
+		} else if idx%int(math.Ceil(float64(linecount)/500.0)) == 0 {
+			valididxMap[idx] = 1
+		}
+	}
+	if verbose > 0 {
+		fmt.Println("validMap", valididxMap)
+	}
+
+	//match Top
+	if linecount > 0 {
+		for idx := 0; idx < len(sd.dict); idx++ {
+			var t Result
+			t.Language = sd.dict[idx].Language
+			t.Score = doCalScore(sd.dict[idx].TopRules, linesofCode[0])
+			if t.Score >= 100 {
+				return t, nil
+			}
+		}
+	}
+
+	//match
+	rs := make([]Result, len(sd.dict))
 	for idx := 0; idx < len(sd.dict); idx++ {
 		var t Result
 		t.Language = sd.dict[idx].Language
-		for jdx := 0; jdx < len(sd.dict[idx].Rules); jdx++ {
-			if sd.dict[idx].Rules[jdx].exp.Match(txt) {
-				t.Score += sd.dict[idx].Rules[jdx].Score
+		for lineidx, _ := range valididxMap {
+			if neartop(lineidx) {
+				t.Score += doCalScore(sd.dict[idx].NearTopRules, linesofCode[lineidx])
+				if verbose > 1 {
+					fmt.Println(sd.dict[idx].NearTopRules)
+				}
 			}
-			if t.Score <= -1.0 {
-				break
-			}
+			t.Score += doCalScore(sd.dict[idx].Rules, linesofCode[lineidx])
 		}
+		rs[idx] = t
 	}
+
 	var maxIdx int
-	for idx := 1; idx < len(r); idx++ {
-		if r[idx].Score > r[maxIdx].Score {
+	for idx := 1; idx < len(rs); idx++ {
+		if rs[idx].Score > rs[maxIdx].Score {
 			maxIdx = idx
 		}
 	}
+	if verbose > 0 {
+		fmt.Println(rs)
+	}
 
-	return r[maxIdx], nil
+	if rs[maxIdx].Score < 2 {
+		rs[maxIdx].Language = "unknown"
+		return rs[maxIdx], ErrNotMatch
+	}
+	return rs[maxIdx], nil
 }
 
 func main() {
@@ -161,21 +234,21 @@ func main() {
 	//fmt.Println("Indexs:", det.GetRuleIndex())
 	var dcount, ndcount float64
 	t := time.Now()
+
+	language := "jsp"
 	utils.DoWalkDir("E:/centosshare/webshell/WebSHArk/WebSHArk", "", func(fileName string, isdir bool) error {
-		if !isdir && strings.HasSuffix(fileName, "") {
+		if !isdir && strings.HasSuffix(fileName, language) {
 			txt, err := ioutil.ReadFile(fileName)
 			if err != nil {
 				fmt.Println(err)
 			} else {
-				r, err := det.DetectAll(txt)
-				if err != nil {
-					fmt.Println(fileName, err)
+				r, _ := det.DetectBest(txt)
+				if r.Language != language {
 					ndcount += 1
+					fmt.Println(fileName, r)
 				} else {
-					ndcount += 1
-					if len(r) > 1 {
-						fmt.Println(fileName, r)
-					}
+					dcount += 1
+					//fmt.Println("[OK]", fileName, r)
 				}
 			}
 		}
--- rule.go
+++ rule.go
@@ -6,13 +6,15 @@ import (
 
 type RuleItem struct {
 	Value string         `json:"value" yaml:"value"`
-	Score float64        `json:"score" yaml:"score"`
+	Score int            `json:"score" yaml:"score"`
 	exp   *regexp.Regexp `-`
 }
 
 type Rule struct {
-	Language string     `json:"language" yaml:"language"`
-	Rules    []RuleItem `json:"rules" yaml:"rules"`
+	Language     string     `json:"language" yaml:"language"`
+	TopRules     []RuleItem `json:"top" yaml:"top"`
+	NearTopRules []RuleItem `json:"neartop" yaml:"neartop"`
+	Rules        []RuleItem `json:"rules" yaml:"rules"`
 }
 
 type Rules []Rule
